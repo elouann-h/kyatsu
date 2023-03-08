aa4ce5ce9c9d82c3f3619e19e0f8a1e5f8c17455
PxndxDev <elouannhosta01@gmail.com>
2023-03-08 20:30:35 +0100

    Built the Client initializer

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,7 +1,58 @@
-import { Client, ClientOptions } from "discord.js";
+import { Client, ClientOptions, GatewayIntentBits, Collection } from "discord.js";
+import * as fs from "fs";
+const path = require("path");
 
-export class KyaClient extends Client {
-  constructor(options: ClientOptions) {
+interface KyaOptions extends ClientOptions {
+  eventsDir?: string | undefined,
+  commandsDir?: string | undefined,
+  token?: string | undefined,
+}
+
+class KyaClient extends Client {
+  private eventsDir: string | undefined;
+  private commandsDir: string | undefined;
+  private commands: Collection<string, any>;
+
+  constructor(options: KyaOptions) {
     super(options);
+    this.eventsDir = options.eventsDir;
+    this.commandsDir = options.commandsDir;
+
+    this.commands = new Collection();
   }
+
+  set setEventsDir(dir: string | undefined) {
+    this.eventsDir = dir;
+  }
+
+  set setCommandsDir(dir: string | undefined) {
+    this.commandsDir = dir;
+  }
+
+  public async login(token?: string): Promise<string> {
+    return super.login(token);
+  }
+
+  public loadEvents(commandsDir?: string) {
+    if (commandsDir) this.commandsDir = commandsDir;
+    if (!this.commandsDir) throw new Error("No commands directory provided.");
+
+    for (const file of fs.readdirSync(this.commandsDir)) {
+      const command: any = require(`${this.commandsDir}/${file}`);
+      console.log(command);
+    }
+  }
+}
+
+export function create(options: KyaOptions | string | any) {
+  const defaultOptions: KyaOptions = {
+    failIfNotExists: false,
+    intents: [GatewayIntentBits.Guilds],
+    eventsDir: "./events",
+    commandsDir: "./commands",
+  }
+  if (typeof options === "string") {
+    defaultOptions.token = options;
+  }
+  return new KyaClient(Object.assign(defaultOptions, options));
 }
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 import * as Discord from "discord.js";
 // @ts-ignore
 import * as DiscordAPITypes from "discord-api-types";
+import * as Client from "./client/client";
 
-export * as Client from "./client/client";
+export {
+  Client,
+}
